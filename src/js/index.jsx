90cde27295b06802585ce5800dac355fbafba35b
Enda Quigley <endaquigley@gmail.com>
2015-10-08 06:47:51 +0100

    upgrade to react 0.14

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Application from './components/application.jsx';
 
-React.render(<Application />, document.getElementById('main'));
+ReactDOM.render(<Application />, document.getElementById('main'));
